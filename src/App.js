3a95fe28fd62e8254f1549d6021c9cae6c479bfe
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-11-12 23:24:20 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
+let bookList = [];
 function App() {
+  const [bookComponents, setBooks] = useState(null);
+
+  let apiUrl = async (term) => {
+    var postResp = await fetch(
+      `https://goodreads-server-express--dotdash.repl.co/search/${term}`
+    );
+
+    bookList = await postResp.json();
+  };
+
+  async function doSomething(event) {
+    var searchItem = document.getElementById("un").value;
+    console.log(searchItem);
+
+    await apiUrl(searchItem).then(() => {
+      console.log(bookList.list);
+
+      setBooks(
+        bookList.list.map((book, index) => {
+          return <BookComponent key={index} book={book} />;
+        })
+      );
+    });
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form>
+          <input type="text" id="un" />
+          <button type="submit" onClick={(event) => doSomething(event)}>
+            Search
+          </button>
+        </form>
+
+        {bookComponents &&
+          bookComponents.map((book) => {
+            return book;
+          })}
+      </div>
     </div>
   );
 }
 
+function BookComponent(props) {
+  return (
+    <div className="column">
+      <div className="book">
+        <div>{props.book.authorName}</div>
+        <img src={props.book.imageUrl} />
+        <div>{props.book.title}</div>
+      </div>
+    </div>
+  );
+}
 export default App;
